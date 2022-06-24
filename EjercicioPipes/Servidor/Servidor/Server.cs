7406806a74e2e51f9ebd8912911c5c4bcd950831
Andrés <ad.pereda@usp.ceu.es>
2022-06-24 15:59:50 +0200

    Commit Andrés

---
+++ EjercicioPipes/Servidor/Servidor/Server.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+namespace Servidor
+{
+    public class Server
+    {
+        public void conectar()
+        {
+            NamedPipeServerStream nmp = new NamedPipeServerStream("Servidor");
+            Console.WriteLine("Esperando conexion...");
+            nmp.WaitForConnection();
+            Console.WriteLine("Conectado");
+            Console.ReadKey();
+        }
+    }
+}
