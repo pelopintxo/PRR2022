548f765c2d9d6fc33e0c618079ca76e61553a786
Andrés <ad.pereda@usp.ceu.es>
2022-06-24 15:51:21 +0200

    Ejercicio 3 Andrés

---
+++ EjercicioPipes/Cliente/Cliente/Client.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+
+namespace Cliente
+{
+    public class Client
+    {
+        public void conectar()
+        {
+            NamedPipeClientStream nmp = new NamedPipeClientStream("Servidor");
+            nmp.Connect();
+            Console.WriteLine("Conectado con el servidor");
+
+
+            Console.ReadKey();
+        }
+    }
+}
